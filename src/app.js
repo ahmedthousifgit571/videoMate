dfb58bf3f5477a23ce4085996de1ef0bd01fdd09
ahmed thousif <ahmedthousif571@gmail.com>
2024-09-18 01:09:56 +0530

    setting jwt token

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
-import express from 'express';
-import CORS from 'cors'
-
+import express from "express";
+import CORS from "cors";
+// import crypto from"crypto";
 const app = express();
 
 app.use(
-    CORS({
-        origin:process.env.CORS_ORIGIN,
-        credentials:true
-    })
-)
+  CORS({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, 
+  })
+);
 // common middleware
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static('public'))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-
+// function generateAccessToken(length) {
+//   return crypto.randomBytes(length).toString("hex"); // Convert to a hexadecimal string
+// }
+// const token = generateAccessToken(32); // Generates a 64-character token
+// console.log("Generated Access Token:", token);
 
 //import routes
-import  healthcheckRouter  from './controllers/healthCheck.controller.js';
+import healthcheckRouter from "./controllers/healthCheck.controller.js";
 
 //routes
-app.use("/api/v1/healthcheckup",healthcheckRouter)
-export  { app };
+app.use("/api/v1/healthcheckup", healthcheckRouter);
+export { app };
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,5 @@
-
-
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -48,5 +47,36 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("Save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+     process.env.ACCESS_TOKEN_SECRET,
+     {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+ )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign({
+        _id:this._id,
+    },
+     process.env.REFRESH_TOKEN_SECRET,
+     {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+ )
+}
 
 export const User = mongoose.model("User", userSchema);
